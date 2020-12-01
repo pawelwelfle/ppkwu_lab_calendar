d1b33d910d5fb5e754147fbfc77cf5e8d1b68854
Paweł Welfle <pawel.welfle@gmail.com>
2020-12-01 15:02:56 +0100

    Prepare weeia calendar event get method

--- src/main/java/ppkwu/lab/calendar/CalendarController.java
+++ src/main/java/ppkwu/lab/calendar/CalendarController.java
@@ -1,19 +1,27 @@
 package ppkwu.lab.calendar;
 
 import org.springframework.web.bind.annotation.*;
+import biweekly.ICalendar;
 
 @RestController
 public class CalendarController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public static String getCalendarURL(@RequestParam("year") String year, @RequestParam("month") String month) {
-        StringBuilder urlBuilder = new StringBuilder();
-        urlBuilder.append("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=");
-        urlBuilder.append(year);
-        urlBuilder.append("&miesiac=");
-        urlBuilder.append(month);
-        urlBuilder.append("&lang=01");
+    @GetMapping("/calendar")
+    @ResponseBody
+    public static String getCalendarURL(@RequestParam("year") String year, @RequestParam("month") String month){
+        String urlBuilder = new String();
+        urlBuilder.concat("http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=");
+        urlBuilder.concat(year);
+        urlBuilder.concat("&miesiac=");
+        urlBuilder.concat(month);
+        urlBuilder.concat("&lang=01");
+        ICalendar cal = new ICalendar();
+        cal.setExperimentalProperty("X-WR-CALNAME", "Calendar");
 
-        return urlBuilder.toString();
+     return urlBuilder;
     }
+
+//    private List<Event> getWeeiaCalendar(String url){
+//        return ;
+//    }
 }
---
+++ src/main/java/ppkwu/lab/calendar/Event.java
@@ -0,0 +1,6 @@
+package ppkwu.lab.calendar;
+
+public class Event {
+    private int day;
+    String name;
+}
